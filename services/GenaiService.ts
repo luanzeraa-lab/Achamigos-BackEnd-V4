abbe7c9250ffe2cccd7805f8d5b71b914405b1ab
Luan Silva de Picoli <luansilva_2@hotmail.com>
2026-03-24 13:15:20 -0300

    fix: arrumando sequencias de tags

--- services/GenaiService.ts
+++ services/GenaiService.ts
@@ -7,7 +7,7 @@ export async function gerarTexto(prompt: string): Promise<string> {
     });
 
     const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
+      model: "gemini-3-flash-preview",
       contents: prompt,
     });
 
