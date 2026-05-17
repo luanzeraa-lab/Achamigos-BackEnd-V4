61cccb9dc4b231740e1487c68e4bf61c1f70121c
Luan Silva de Picoli <luansilva_2@hotmail.com>
2026-05-17 10:10:14 -0300

    fix(ci): adiciona mapeamento de cobertura de testes para o sonar

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ export default {
     restoreMocks: true,
     collectCoverageFrom: ['**/*.ts'],
     coverageDirectory:'coverage',
+    coverageReporters: ['text', 'lcov'],
     collectCoverage: true,
     coverageThreshold: {
         global: {
