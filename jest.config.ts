deaeb27a38e56057bf0159d470c751a375d532f8
Luan Silva de Picoli <luansilva_2@hotmail.com>
2026-05-26 22:23:52 -0300

    Merge branch 'feature/testes' resolvendo conflitos

--- jest.config.ts
+++ jest.config.ts
@@ -15,42 +15,16 @@ export default {
     coverageDirectory:'coverage',
     collectCoverage: true,
     modulePathIgnorePatterns: ['<rootDir>/dist/'],
-    collectCoverageFrom: [
-        'controllers/**/*.ts',
-        'models/**/*.ts',
-        'services/**/*.ts',
-        'routes/**/*.ts',
-        'middleware/**/*.ts',
-        'utils/**/*.ts',
-        'config/**/*.ts',
-        'api.ts'
-    ],
-    coveragePathIgnorePatterns: [
-        '/node_modules/',
-        '/dist/',
-        '/coverage/',
-        'swagger.ts',
-        '.spec.ts',
-        '.test.ts',
-        '.test.js'
-    ],
     testPathIgnorePatterns: [
         '/node_modules/',
         '/dist/'
     ],
     coverageThreshold: {
         global: {
-<<<<<<< HEAD
-            branches: 5,
-            statements: 5,
-            lines: 5,
-            functions: 5
-=======
             branches: 80,
             statements: 80,
             lines: 80,
             functions: 80
->>>>>>> feature/testes
         }
     },
     // Normalize paths for SonarQube (Windows compatibility)
